4239e0b560ab684a02f36f9d638dcb9e60b51f1c
darkfos82 <darkfos82@gmail.com>
2024-11-09 02:58:00 +0300

    add: crud operations on user model

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,7 @@
 const dbConnection = require("../database/dbWorker");
+const authConfig = require("../configs/authConfig");
+const jwt = require("jsonwebtoken");
+const crypto = require("../services/hash");
 
 
 class UserController {
@@ -21,15 +24,58 @@ class UserController {
     }
 
     allInfromationAboutUser(req, res) {
-
+        let token = req.headers.authorization;
+        let checkToken = this.checkToken(token.split(" ")[1]);
+        if (!token && checkToken === false) {
+            return res.status(401).json({"message": "Незарегистрированный пользователь"});
+        }
+        dbConnection.query(`SELECT * FROM "User" WHERE id=${checkToken.sub}`).then((r) => {
+            let result = r.rows[0];
+            return res.status(200).json({
+                name: result.name,
+                email: result.email
+            })
+        }).catch((e) => { return res.status(400).send("Пользователь не был найден") })
     }
 
     updatePassword(req, res) {
+        let token = req.headers.authorization;
+        let toUpdate = req.body;
+        let checkToken = this.checkToken(token.split(" ")[1]);
 
+        if (!token || checkToken === false || toUpdate.password === undefined) {
+            return res.status(401).json({"message": "Незарегистрированный пользователь"});
+        }
+        
+        crypto.hashedPassword(toUpdate.password).then(password => {
+            dbConnection.query(`UPDATE "User" SET hashed_password='${password}' WHERE id=${checkToken.sub}`).then((r) => {
+                return res.status(200).send("Пароль был успешно обновлен");
+            }).catch((error) => { res.status(400).send("Не удалось обновить пароль") });
+        }).catch((er) => {res.status(400).send("Не удалось провести шифрование пароля")});
     }
 
     deactivateAccount(req, res) {
+        let token = req.headers.authorization;
+        let toUpdate = req.body;
+        let checkToken = this.checkToken(token.split(" ")[1]);
 
+        if (!token || checkToken === false || toUpdate.password === undefined) {
+            return res.status(401).json({"message": "Незарегистрированный пользователь"});
+        }
+
+        dbConnection.query(`DELETE FROM "User" WHERE id=${checkToken.sub}`).then((r) => {
+            return res.status(200).send("Пользователь удалил свой аккаунт");
+        }).catch((e) => {res.status(400).send("Пользователь не смог удалить аккаунт")})
+    }
+
+    checkToken(token) {
+        try {
+            if (jwt.verify(token, authConfig.tokenKey)) {
+                return jwt.decode(token)
+            } return false
+        } catch (er) {
+            return false
+        }
     }
 }
 
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -5,7 +5,7 @@ const userController = require("../controllers/userController");
 
 // Endpoint's
 userRouter.get("/information_about_me", userController.allInfromationAboutUser);
-userRouter.post("/updatePassword", userController.updatePassword);
+userRouter.put("/updatePassword", userController.updatePassword);
 userRouter.delete("/deleteMe", userController.deactivateAccount);
 
 
